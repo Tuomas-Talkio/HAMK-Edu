c3f5d07520090a1e1904a9239c0c2e6df05310d0
gibanator <georgy.gibizov@gmail.com>
2023-09-16 00:42:18 +0300

    added speedometers representing the average group answers
    also, all the values and the number of the charts is dynamic

--- results.php
+++ results.php
@@ -2,27 +2,50 @@
 include 'includes/header.php'; ?>
 
 <main>
+    <script src="speedometer.js"></script>
     <section>
         <h2>Results</h2>
         <?php
         include ('connect_to_db.php');
-        $result = mysqli_query($conn, "select * from results where id = '5'");
+        $result = mysqli_query($conn, "SHOW COLUMNS FROM results");
 
-        echo "<table>";
-        echo "<tr><th>ID</th><th>Process</th><th>Env</th></tr>";
-
+        $columnNames = array();
         while ($row = $result->fetch_assoc()) {
-            echo "<tr>";
-            echo "<td>" . $row['id'] . "</td>";
-            echo "<td>" . $row['process'] . "</td>";
-            echo "<td>" . $row['enviroment'] . "</td>";
-            echo "</tr>";
+            if ($row['Field'] != 'id' && $row['Field'] != 'peergroup') {
+                $columnNames[] = $row['Field'];
+            }
         }
 
-        echo "</table>";
 
+        $columnList = implode(', ', $columnNames);
+        $result = mysqli_query($conn,"SELECT " . $columnList . " FROM results");
+
+        if ($result->num_rows > 0) {
+            $averages = array_fill_keys($columnNames, 0);
+            $rowCount = 0;
+
+            while ($row = $result->fetch_assoc()) {
+                foreach ($columnNames as $columnName) {
+                    $averages[$columnName] += $row[$columnName];
+                }
+                $rowCount++;
+            }
+
+            // Calculate the averages
+            foreach ($averages as $columnName => $sum) {
+                $averages[$columnName] /= $rowCount;
+            }
+            foreach ($averages as $columnName => $averageValue) {
+                echo "<p>$columnName</p>";
+                echo "<canvas id='$columnName' class='speedometer-canvas' data-result='$averageValue' width='300' height='300'></canvas>";
+            }
+        }
+        else {
+                echo "No results found";
+        }
+
+        $conn->close();
         ?>
-
     </section>
 </main>
 
