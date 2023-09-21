94a99b0094037fc654fc7927f5da6040c8fb2391
gibanator <georgy.gibizov@gmail.com>
2023-09-21 11:33:51 +0300

    small changes to style

--- includes/header.php
+++ includes/header.php
@@ -7,6 +7,7 @@ session_start()
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <title>Group1213</title>
 </head>
@@ -16,7 +17,7 @@ session_start()
 </header>
 <nav>
     <ul>
-        <li><a href="#">Home</a></li>
+        <li><a href="../index.php">Home</a></li>
         <li><a href="#">About</a></li>
         <li><a href="#">Contact</a></li>
     </ul>
